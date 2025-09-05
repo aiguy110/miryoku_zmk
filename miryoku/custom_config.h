c607dbad15bebbb3e1712314f4ffbb042da2a29a
Josiah Hunsinger <fencepost13302@gmail.com>
2025-09-05 09:15:06 -0400

    Configure ZMK keyboard name and sleep timeout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,9 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// Sleep timeout: 2 minutes (120000 ms)
+#define CONFIG_ZMK_IDLE_SLEEP_TIMEOUT 120000
+
+// Bluetooth device name
+#define CONFIG_ZMK_KEYBOARD_NAME "Josiah_Chocofi"
+
